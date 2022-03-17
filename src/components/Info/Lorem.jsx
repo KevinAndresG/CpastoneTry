2dc92a0e151afc1cd0ea6d77fced0f3ccdd46c7c
KevinAndresG <3348@holbertonschool.com>
2022-03-17 12:24:18 -0500

    changing some things

--- src/components/Info/Lorem.jsx
+++ src/components/Info/Lorem.jsx
@@ -2,17 +2,11 @@ import React from "react"
 import "../styles/index.css"
 function Lorem() {
 	return (
-		<div class="divLorem">
-			<p class="lorem">
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br>
-				1500s, when an unknown printer took a galley of type and scrambled it to<br></br>
-				make a type specimen book. It has survived not only five centuries,<br></br>
-				but also the leap into electronic typesetting, remaining essentially<br></br>
-				unchanged. It was popularised in the 1960s with the release of Letraset<br></br>
-				sheets containing Lorem Ipsum passages, and more recently with desktop<br></br>
-				publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
-			</p>
-		</div>
+		<code class="lorem parplorem">
+			with HiSsages you can choise the encrypting method<br></br>
+			that you prefer for more security<br></br>
+			from end to end to your messages
+		</code>
 	)
 }
 export { Lorem }
--- src/components/login/Lorem.jsx
+++ src/components/login/Lorem.jsx
@@ -3,15 +3,12 @@ import "../styles/index.css"
 function Lorem() {
 	return (
 		<div class="divLorem">
-			<p class="lorem">
-				Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br>
-				1500s, when an unknown printer took a galley of type and scrambled it to<br></br>
-				make a type specimen book. It has survived not only five centuries,<br></br>
-				but also the leap into electronic typesetting, remaining essentially<br></br>
-				unchanged. It was popularised in the 1960s with the release of Letraset<br></br>
-				sheets containing Lorem Ipsum passages, and more recently with desktop<br></br>
-				publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
-			</p>
+			<code class="lorem parplorem">
+			with HiSsages you can choise<br></br>
+			the encrypting method<br></br>
+			that you prefer for more security<br></br>
+			from end to end to your messages
+			</code>
 		</div>
 	)
 }
